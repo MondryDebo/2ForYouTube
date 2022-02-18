6d9870fdeb9e3eae459ab84106b2c7c14020ddac
MondryD3bo <tymondebicki6@gmail.com>
2022-02-18 11:17:15 +0100

    Add basic video details to thr app

---
+++ src/components/Download.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="downloadWrapper">
+    <img :src="thumbnail" :alt="title" width="350" class="thumbnail">
+    <div class="right">
+      <h3 class="title">{{ title }}</h3>
+      <p class="author">{{ author }}</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Download',
+  props: {
+    results: {
+      type: Object,
+      required: true,
+    }
+  },
+  data() {
+    return {
+      thumbnail: null,
+      title: null,
+      author: null,
+    };
+  },
+  mounted() {
+    this.thumbnail = this.results.videoDetails.thumbnail;
+    this.title = this.results.videoDetails.title;
+    this.author = this.results.videoDetails.author;
+  },
+}
+</script>
+
+<style lang="scss" scoped>
+  .downloadWrapper {
+    display: flex;
+    width: 350px;
+
+    .right {
+      display: flex;
+      flex-direction: column;
+      text-align: left;
+      margin-left: 15px;
+
+      .title {
+        width: 550px;
+        color: black;
+        font-weight: 600;
+      }
+    }
+
+    .thumbnail {
+      border-radius: 5px;
+    }
+  }
+</style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -8,9 +8,10 @@
           <SearchInput v-model="searchValue" />
           <SearchButton @search="handleSearch" />
         </div>
-        <div class="results" v-if="!loading">
-          <p class="title">{{ results.videoDetails.title }}</p>
+        <div class="results" v-if="results.loaded && !loading">
+          <Download :results="results" />
         </div>
+        <div class="loader" v-if="loading"><div></div><div></div><div></div><div></div></div>
       </div>
     </div>
   </div>
@@ -20,29 +21,43 @@
 import axios from 'axios';
 import SearchInput from '@/components/SearchInput.vue';
 import SearchButton from '@/components/SearchButton.vue';
+import Download from '@/components/Download.vue';
 
 export default {
   name: 'Home',
   components: {
     SearchInput,
     SearchButton,
+    Download,
   },
   data() {
     return {
       loading: false,
       searchValue: '',
       results: {
+        loaded: false,
         videoDetails: {
           title: '',
+          thumbnail: '',
+          author: '',
         }
       },
     };
   },
   methods: {
     async handleSearch() {
-      const res = await axios.get(`https://youtube-api-nodejs.herokuapp.com/apiVideo?URL=${this.searchValue}`);
-      const data = res.data;
-      this.results.videoDetails.title = data.videoDetails.title;
+      this.loading = true;
+      await axios.get(`https://youtube-api-nodejs.herokuapp.com/apiVideo?URL=${this.searchValue}`)
+        .then(res => {
+          this.results.loaded = true;
+          this.loading = false;
+          const data = res.data;
+          console.log(data);
+          this.results.videoDetails.title = data.videoDetails.title;
+          this.results.videoDetails.thumbnail = data.videoDetails.thumbnails[4].url;
+          this.results.videoDetails.author = data.videoDetails.author.name;
+        })
+        .catch(err => console.error(err));
     },
   },
 }
@@ -55,7 +70,7 @@ export default {
   }
 
   .videoWrapper {
-    margin-top: 50px;
+    margin: 50px;
     border: 1px solid rgba(0, 0, 0, 0.1);
     width: 75%;
     height: auto;
@@ -73,5 +88,55 @@ export default {
     margin: 20px;
     display: flex;
     justify-content: center;
+  }
+
+  .results {
+    display: flex;
+    margin-top: 25px;
+    width: 95%;
+    background: rgba(238, 238, 238, 0.562);
+    padding: 20px;
+    border-radius: 15px;
+  }
+
+  .loader {
+    display: inline-block;
+    position: relative;
+    width: 64px;
+    height: 64px;
+    margin-top: 50px;
+    @media (min-width: 768px) {
+      width: 90px;
+      height: 90px;
+    }
+  }
+  .loader div {
+    box-sizing: border-box;
+    display: block;
+    position: absolute;
+    width: 64px;
+    height: 64px;
+    margin: 8px;
+    border: 8px solid #1e3d4a;
+    border-radius: 50%;
+    animation: loader 1.2s cubic-bezier(0.5, 0, 0.5, 1) infinite;
+    border-color: #1e3d4a transparent transparent transparent;
+  }
+  .loader div:nth-child(1) {
+    animation-delay: -0.45s;
+  }
+  .loader div:nth-child(2) {
+    animation-delay: -0.3s;
+  }
+  .loader div:nth-child(3) {
+    animation-delay: -0.15s;
+  }
+  @keyframes loader {
+    0% {
+      transform: rotate(0deg);
+    }
+    100% {
+      transform: rotate(360deg);
+    }
   }
 </style>
